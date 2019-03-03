784af5942b1b3cf29c3746c1c8a11f0e241e8591
Mishon <terem848@gmail.com>
2019-03-03 11:48:32 +0300

    #14

--- public/script.js
+++ public/script.js
@@ -1,18 +1,18 @@
-let sub = document.getElementById('sub');
-  sub.onclick  = function () {
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", "/reg", true);
+let regButton = document.getElementById('regButton');
+regButton.onclick = function () {
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "/newUser", true);
+  xhr.setRequestHeader('Content-Type', 'application/json;charset=utf-8');
+  xhr.send(JSON.stringify({
+    email: email.value,
+    password: password.value,
+    displayname: displayName.value
+  }));
+  
+  email.value = '';
+  password.value = '';
+  displayName.value = '';
 
-    xhr.setRequestHeader('Content-Type', 'application/json;charset=utf-8');
-    xhr.send(JSON.stringify({
-      email: exampleInputEmail1.value,
-      pass: exampleInputPassword1.value,
-      displayname: DisplayName.value
-    }));
+  return false;
+};
 
-    this.elements.message.value = '';
-
-    return false;
-  };
-
- 
