21aa40e9945e563cf676b4e424e267f2c56bdf87
Mishon <terem848@gmail.com>
2019-03-03 09:56:38 +0300

    #12

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const router = new Router();
 require('./handlers/01_errorHandler').init(app);
 require('./handlers/02_templates').init(app);
 require('./handlers/03_static').init(app);
+require('./handlers/04_bodyparser').init(app);
 
 router.get('/login', require('./routers/login').get);
 router.get('/registration', require('./routers/registration').get);
