309c116a1c3662dae9c56c46828d4717c2de79d3
Mishon-pon-pon <terem848@gmail.com>
2020-12-06 08:52:04 +0300

    #57 login page

--- routers/RegAndLog/frontPage.js
+++ routers/RegAndLog/frontPage.js
@@ -1,11 +1,15 @@
 const fs = require('fs');
 const config = require('config')
+const static = require('koa-static');
+const serve = require('koa-static');
 
 exports.get = async (ctx, next) => {
     if (ctx.isAuthenticated()) {
         ctx.type = 'html'
         ctx.body = ctx.htmlrender(config.get('public') + '/app.html');
     } else {
-        ctx.body = ctx.render('login.pug')
+        // ctx.body = ctx.render('login.pug')
+        ctx.type = 'html';
+        ctx.body = ctx.htmlrender(config.get('public') + '/index.html');
     }
 };
